dd7acd96fe2bf9d2516431bc591343e969246396
Lakshmanan D <56861446+lakshdhamo@users.noreply.github.com>
2021-12-06 10:15:57 +0530

    Init Only Setters for AssetDetailVm

--- Hahn.ApplicatonProcess.July2021.API/Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.July2021.Domain/ViewModels/AssetDetailVm.cs
+++ Hahn.ApplicatonProcess.July2021.API/Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.July2021.Domain/ViewModels/AssetDetailVm.cs
@@ -10,39 +10,39 @@ namespace Hahn.ApplicatonProcess.July2021.Domain.VMs
     public class AssetDetailVm : AssetVm
     {
         [JsonProperty("id")]
-        public string Id { get; set; }
+        public string Id { get; init; }
 
         [JsonProperty("rank")]
-        public string Rank { get; set; }
+        public string Rank { get; init; }
 
         //[JsonProperty("symbol")]
-        //public string Symbol { get; set; }
+        //public string Symbol { get; init; }
 
         //[JsonProperty("name")]
-        //public string Name { get; set; }
+        //public string Name { get; init; }
 
         [JsonProperty("supply")]
-        public string Supply { get; set; }
+        public string Supply { get; init; }
 
         [JsonProperty("maxSupply")]
-        public string MaxSupply { get; set; }
+        public string MaxSupply { get; init; }
 
         [JsonProperty("marketCapUsd")]
-        public string MarketCapUsd { get; set; }
+        public string MarketCapUsd { get; init; }
 
         [JsonProperty("volumeUsd24Hr")]
-        public string VolumeUsd24Hr { get; set; }
+        public string VolumeUsd24Hr { get; init; }
 
         [JsonProperty("priceUsd")]
-        public string PriceUsd { get; set; }
+        public string PriceUsd { get; init; }
 
         [JsonProperty("changePercent24Hr")]
-        public string ChangePercent24Hr { get; set; }
+        public string ChangePercent24Hr { get; init; }
 
         [JsonProperty("vwap24Hr")]
-        public string Vwap24Hr { get; set; }
+        public string Vwap24Hr { get; init; }
 
         [JsonProperty("explorer")]
-        public string Explorer { get; set; }
+        public string Explorer { get; init; }
     }
 }
